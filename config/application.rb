d15d8083bee59a94482bd8d0fc64cc7b44c7a841
parkerdan <parker.daniel.o@gmail.com>
2016-10-14 17:50:31 -0500

    fix

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,7 @@ module OnTapServer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.serve_static_files = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
