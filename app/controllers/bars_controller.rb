fdd12eb463b677e43bcceb0d80472359c69c0827
parkerdan <parker.daniel.o@gmail.com>
2016-10-14 17:14:19 -0500

    image fix?

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -16,10 +16,10 @@ class BarsController < ApplicationController
   def show
     # @taps = Tap.where(bar_id: @bar.id)
     if user_signed_in? && @bar.user_id == current_user.id
-      @taps = Tap.where("bar_id = ?", @bar.id)
+      @taps = Tap.where("bar_id = ?", @bar.id).order(:created_at)
     else
       # @tap = Tap.find(params[:id])
-      @taps = Tap.where("bar_id = ? AND show_card = ?", @bar.id, true)
+      @taps = Tap.where("bar_id = ? AND show_card = ?", @bar.id, true).order(:created_at)
     end
 
   end
