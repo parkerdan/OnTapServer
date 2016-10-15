905967c84c8d06145035772f27c957561755655a
parkerdan <parker.daniel.o@gmail.com>
2016-10-15 11:31:15 -0500

    ribbon/modal

--- public/assets/application-ab6a43c3bf0120af42ebb7690710671d064ed513a2925e4438194297d12cef63.js
+++ public/assets/application-ab6a43c3bf0120af42ebb7690710671d064ed513a2925e4438194297d12cef63.js
@@ -15385,10 +15385,30 @@ document.addEventListener(listener, function(){
 });
 //  bundle exec rake release
 ;
-(function() {
+
+var listener = (window.Turbolinks) ? "page:change":"DOMContentLoaded";
+
+function addRecipeClick(element){
+  var recipe = $(element).data().recipe;
+  element.addEventListener('click',function(){
+    document.getElementById("recipe-modal-text").innerHTML = recipe.replace(/\n\r?/g, '<br />');
+  });
+};
 
 
-}).call(this);
+document.addEventListener(listener, function(){
+
+  if (document.getElementsByClassName("ribbon").length > 0) {
+
+    var ribbons = document.getElementsByClassName("ribbon");
+
+    for (var i = 0; i < ribbons.length; i++) {
+      addRecipeClick(ribbons[i])
+    }
+
+  }
+
+});
 (function() {
 
 
