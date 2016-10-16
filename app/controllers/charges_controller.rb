d8245823c09285dd8e658d6c2f27b5599400a6f1
parkerdan <parker.daniel.o@gmail.com>
2016-10-15 22:14:27 -0500

    stripe integration going well

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -3,37 +3,47 @@ class ChargesController < ApplicationController
   def new
   end
 
-  def create(i)
+  def create
     # Amount in cents
-    if i == 1
-      customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-      )
+      if params[:stripeTokenOneTime]
+          @amount = params[:donation_amount].to_i
 
-      charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => '500',
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-      )
-    else
-      customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
-      )
+          @text = "Thanks, you've made a one time payment of "
+          # Stripe::Charge.create(
+          #   :amount => (@amount * 100),
+          #   :currency => 'usd',
+          #   :source => params[:stripeTokenOneTime],
+          #   :description => 'Custom Donation'
+          # )
+      elsif params[:stripeTokenSubscription]
+        puts "***************************   SUBSCRIPTION  *****************************************"
+        puts params.inspect
 
-      charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => '500',
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
-      )
-    end
+        @amount = params[:subscribe][:monthly].to_i
+        @text = "Thanks, you\'ve signed up for a monthly donation of "
+      end
 
-  rescue Stripe::CardError => e
-    flash[:error] = e.message
-    redirect_to new_charge_path
+      # customer = Stripe::Customer.create(
+      # :email => params[:stripeEmail],
+      # :source  => params[:stripeToken]
+      # )
+
+      # customer = Stripe::Customer.create(
+      # :email => params[:stripeEmail],
+      # :source  => params[:stripeToken]
+      # )
+      #
+      # charge = Stripe::Charge.create(
+      # :customer    => customer.id,
+      # :amount      => '500',
+      # :description => 'Rails Stripe customer',
+      # :currency    => 'usd'
+      # )
+
+  # rescue Stripe::CardError => e
+  #   flash[:error] = e.message
+  #   redirect_to new_charge_path
+
   end
-  
+
 end
