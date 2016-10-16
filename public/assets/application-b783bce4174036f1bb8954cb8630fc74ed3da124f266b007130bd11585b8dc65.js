b7d957e95c95cb6ac86d0111555763f9c1d06908
parkerdan <parker.daniel.o@gmail.com>
2016-10-16 16:12:17 -0500

    stripe

--- public/assets/application-b783bce4174036f1bb8954cb8630fc74ed3da124f266b007130bd11585b8dc65.js
+++ public/assets/application-b783bce4174036f1bb8954cb8630fc74ed3da124f266b007130bd11585b8dc65.js
@@ -15389,9 +15389,11 @@ document.addEventListener(listener, function(){
 var listener = (window.Turbolinks) ? "page:change":"DOMContentLoaded";
 
 function addRecipeClick(element){
-  var recipe = $(element).data().recipe;
+  var recipe = element.getAttribute('data-recipe');
+  var title = element.getAttribute('data-title');
   element.addEventListener('click',function(){
     document.getElementById("recipe-modal-text").innerHTML = recipe.replace(/\n\r?/g, '<br />');
+    document.getElementById("modal-header").innerHTML = title;
   });
 };
 
